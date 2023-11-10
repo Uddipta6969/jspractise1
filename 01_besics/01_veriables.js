239baa35ca47b3a68dacb92248540eac87e42f26
Uddipta6969 <144374276+Uddipta6969@users.noreply.github.com>
2023-11-10 08:12:19 +0000

    datatypes

---
+++ 01_besics/01_veriables.js
@@ -0,0 +1,9 @@
+const accountId =126969
+let accountEmail ="uddipta@gmail.com"
+var accountPassword = "12345"
+accountCity = "kolkata"
+let accountState;
+
+//accountId =2//
+console.table([accountId,accountEmail,accountPassword,accountCity, accountState])
+console.log(accountPassword);
---
+++ 01_besics/02_datatypes.js
@@ -0,0 +1,13 @@
+"use strict";// treat all js code as newer 
+let name= "Uddipta"
+let age=24
+let isloggedin=true
+
+/* number => 2 to power 53
+   bigInt
+   string=""
+   boolean= true/false
+   null= standalone value
+   undefined =
+   symbol( whne we will use react unique)
+    */
