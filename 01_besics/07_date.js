cbecdffd96271e255b36537285cc8ba4bf39a6eb
Uddipta6969 <144374276+Uddipta6969@users.noreply.github.com>
2023-12-13 07:15:10 +0000

    concept of date and time

---
+++ 01_besics/07_date.js
@@ -0,0 +1,12 @@
+//dates
+
+/*let myDate= new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log( typeof myDate);*/
+
+let newDate= new Date()
+console.log(newDate);
+console.log(newDate.getTime());
+console.log(newDate.valueOf());
+console.log(newDate.getMonth());
